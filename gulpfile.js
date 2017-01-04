850db9ee1a53161e435b5102f03b89afe79afe18
rbnrjR <rubanraj.rj@gmail.com>
2017-01-04 16:10:20 +0530

    edited gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,9 @@ var install = require("gulp-install");
 var gulp = require('gulp');
 var eslint= require('gulp-eslint');
 
-gulp.src(['./bower.json', './package.json'])
-    .pipe(install());
-
 gulp.task('lint', () => {
-  return gulp.src(['main.js','!node_modules/**'])
+  return gulp.src('main.js')
             .pipe(eslint())
             .pipe(eslint.format())
             .pipe(eslint.failAfterError());
-});
-
-gulp.task('default', ['lint'], () => {
-  gulp.watch('main.js', ['lint']);
 });
