77e1dad22da53be98d7939fb3850ab9bc729b290
GowthamR24 <gowthamcool.gr@gmail.com>
2017-01-04 17:13:22 +0530

    main final

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom';
 import TitleBar from './app/Header.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SearchBar from './app/Search.jsx';
-import {Router,Route,IndexRoute ,hashHistory} from 'react-router';
+import {Router,Route,hashHistory} from 'react-router';
 import Details from './app/details.jsx';
 
 const App = () => (
- <MuiThemeProvider>
- <Router history={hashHistory}>
-   <Route path="/" component={TitleBar}/>
-   <Route path="/details" component={Details}/>
- </Router>
- </MuiThemeProvider>
+  <MuiThemeProvider>
+  <Router history={hashHistory}>
+    <Route path="/" component={SearchBar}/>
+    <Route path="/details/:value" component={Details}/>
+  </Router>
+  </MuiThemeProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('content'));
