b270cbe751f523ce5ea4e2b7d39a5b7d2f63a9e7
Manoj <human.hb@gmail.com>
2017-01-04 15:12:32 +0530

    Update main.js
    Routing not yet working

--- main.js
+++ main.js
@@ -3,19 +3,16 @@ import ReactDOM from 'react-dom';
 import TitleBar from './app/Header.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SearchBar from './app/Search.jsx';
+import {Router,Route,IndexRoute ,hashHistory} from 'react-router';
+import Details from './app/details.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-      <MuiThemeProvider>
-        <div>
-          <TitleBar />
-          <SearchBar />
-        </div>
-      </MuiThemeProvider>
-    );
-  }
-};
-
+const App = () => (
+ <MuiThemeProvider>
+ <Router history={hashHistory}>
+   <Route path="/" component={TitleBar}/>
+   <Route path="/details" component={Details}/>
+ </Router>
+ </MuiThemeProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('content'));
