4d48c179f958652402f6e9633aa7056e038a2637
ariantanay2016 <ariantanay2012@gmail.com>
2017-01-04 15:16:58 +0530

    Update Header.jsx

--- app/Header.jsx
+++ app/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
+import React from 'react';
+import {Link} from 'react-router';
 
 export default class TitleBar extends React.Component {
   render() {
