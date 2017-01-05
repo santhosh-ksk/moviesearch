9a49667f23ae6de2c5a067799420c17bff5a8a06
GowthamR24 <gowthamcool.gr@gmail.com>
2017-01-05 11:00:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TitleBar from './app/Header.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import SearchBar from './app/Search.jsx';
 import {Router,Route,hashHistory} from 'react-router';
@@ -10,7 +9,7 @@ const App = () => (
   <MuiThemeProvider>
   <Router history={hashHistory}>
     <Route path="/" component={SearchBar}/>
-    <Route path="/details/:value" component={Details}/>
+    <Route path="/details/:id" component={Details}/>
   </Router>
   </MuiThemeProvider>
 );
