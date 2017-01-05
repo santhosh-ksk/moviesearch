d96cff6d9420e8830bab3c9fe055c2f38b8f8d56
GowthamR24 <gowthamcool.gr@gmail.com>
2017-01-05 10:57:27 +0530

    Update Search.jsx

--- app/Search.jsx
+++ app/Search.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import ajax from 'superagent';
-import {Pagination, Image} from 'react-bootstrap';
+import {Pagination} from 'react-bootstrap';
 import {Link} from 'react-router';
 import Details from './details.jsx';
 import AppBar from 'material-ui/AppBar';
-
+import {
+    Card,
+    CardActions,
+    CardHeader,
+    CardMedia,
+    CardTitle,
+    CardText
+} from 'material-ui/Card';
+import {Container, Row, Col} from 'react-grid-system';
 
 const style = {
     margin: 12
@@ -19,21 +27,17 @@ class SearchBar extends React.Component {
         this.state = {
             mList: null,
             value: '',
-            items: 0,
             search: false,
-            currentPage:0,
-            results:0
+            currentPage: 1,
+            results: 0
         };
         this.handleChange = this.handleChange.bind(this);
-        this.handleClick = this.handleClick.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
         this.ajaxCall = this.ajaxCall.bind(this);
     }
 
     handleChange(event) {
-        this.setState({
-        value: event.target.value,
-        currentPage: 1
-        });
+        this.setState({value: event.target.value, currentPage: 1});
     }
 
     ajaxCall()
@@ -41,64 +45,69 @@ class SearchBar extends React.Component {
         let url = "http://www.omdbapi.com/?s=" + this.state.value + "&page=" + this.state.currentPage;
         ajax.get(url).end((error, response) => {
             if (response) {
-                console.log(response.body);
+                console.log(response);
                 let movies = response.body.Search;
-                this.setState({search: true, mList: movies, items: movies.length, results:response.body.totalResults,});
+                this.setState({search: true, mList: movies, results: response.body.totalResults});
 
             } else {
                 console.log('There was an error fetching from API', error);
             }
+
         });
 
     }
-    handleClick(event) {
-      let page=event.currentTarget.dataset.id;
-      this.setState({
-        currentPage:page
-      });
-      this.ajaxCall();
+    handleSelect(eventKey) {
+        this.setState({currentPage: eventKey});
+        this.ajaxCall();
     }
-
 
     render() {
         let isSearch = this.state.search;
         let content = null;
+
         if (isSearch) {
-            content = <div>
-                {this.state.mList.map((details, i) => (
-                    <section>
-                       <Link to={"details/"+details.imdbID} key={i}>
-                        <Image src={details.Poster}/>
-                        <h3>Title: {details.Title}</h3>
-                        </Link>
-                    </section>
-                ))}
-            </div>
-   var pages=0
-   if(this.state.results>100)
-   {
-     pages=10
-   }
-   else {
-     pages=Math.ceil(this.state.results/10)
-   }
-   var pagination=[]
-   for(var i=1;i<=pages;i++)
-   {
-     pagination.push(<RaisedButton label={i}  key={i} data-id={i} onClick={this.handleClick}/>)
-   }
+            content = <Container>
+                <Row>
+                    {this.state.mList.map((details, i) => (
+                        <Col lg={4} sm={6} xs={12}>
+                            <Card key={i} style={{
+                                height: "310px",
+                                width: "280px",
+                                marginBottom: "40px"
+                            }}>
+                                <Link to={"details/" + details.imdbID}>
+                                    <CardMedia overlay={< CardTitle title = {
+                                        details.Title
+                                    } />}>
+                                        <img src={details.Poster} style={{
+                                            height: "300px",
+                                            width: "280px",
+                                            marginBottom: "20px"
+                                        }}/>
+                                    </CardMedia>
+                                </Link>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+                <Row>
+                    <center>
+                        <Pagination bsSize="large" prev next first last ellipsis boundaryLinks maxButtons={10} items={Math.ceil(this.state.results / 10)} activePage={this.state.currentPage} onSelect={this.handleSelect}/>
+                    </center>
+                </Row>
+            </Container>
+
         } else {
             content = null;
         }
         return (
-              <div>
-              <AppBar
-                title="MovieFinder"
-              />
-                <TextField hintText="Type Movie Name Here" value={this.state.value} onChange={this.handleChange}/>
-                <RaisedButton label="Search" primary={true} style={style} onClick={this.ajaxCall}/>
+            <div>
+                <AppBar title="MovieFinder"/>
+                <center><TextField hintText="Type Movie Name Here" value={this.state.value} onChange={this.handleChange}/>
+                    <RaisedButton label="Search" primary={true} style={style} onClick={this.ajaxCall}/>
+                </center>
                 {content}
-                {pagination}
+
             </div>
         );
     }
